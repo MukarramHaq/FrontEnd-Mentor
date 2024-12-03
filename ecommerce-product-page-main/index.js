10bc374ee01e4297be5dc6a46eac3e2a68f621ad
MukarramHaq <mukarram796@gmail.com>
2024-12-02 20:12:20 -0500

    Added display cart functionality. I created two carts; empty cart and the filled cart separately. That was not a good idea since it costed me a lot of time and effort and I can't just hide and unhide all the time. I think it would be better if I can get a single cart and then hide the things when the cart is filled maybe? That sounds more doable

--- ecommerce-product-page-main/index.js
+++ ecommerce-product-page-main/index.js
@@ -1,28 +1,33 @@
-const sidebarEl = document.getElementById('sidebar');
-const closeBtnEl = document.getElementById('close-btn');
-const menuBtnEl = document.getElementById('menu-btn')
-const minusBtnEl = document.getElementById('minus-btn');
-const plusBtnEl = document.getElementById('plus-btn');
-const quantityEl = document.getElementById('quantity');
-const cartBtnEl = document.getElementById('cart-btn');
-const cartContainerEl = document.getElementById('cart-container');
+const sidebarEl = document.getElementById("sidebar");
+const closeBtnEl = document.getElementById("close-btn");
+const menuBtnEl = document.getElementById("menu-btn");
+const minusBtnEl = document.getElementById("minus-btn");
+const plusBtnEl = document.getElementById("plus-btn");
+const quantityEl = document.getElementById("quantity");
+const cartBtnEl = document.getElementById("cart-btn");
+const cartContainerEmptyEl = document.getElementById("cart-container-empty");
+const cartContainerFullEl = document.getElementById("cart-container-full");
+const addToCartEl = document.getElementById('add-to-cart');
+const itemsInCartEl = document.getElementById('items-in-cart');
+const totalBillEl = document.getElementById('total');
+const deleteItemEl = document.getElementById('delete-item');
 let productQuantity = 0;
 let currentIndex = 0;
 
 /********** For the sidebar **********/
-closeBtnEl.addEventListener('click', () => {
-    sidebarEl.style.display = 'none'
-})
+closeBtnEl.addEventListener("click", () => {
+  sidebarEl.style.display = "none";
+});
 
-menuBtnEl.addEventListener('click', () => {
-    sidebarEl.style.display = 'flex'
-    sidebarEl.style.boxShadow = '0 0 0 10000px rgba(0, 0, 0, 0.5)';
-})
+menuBtnEl.addEventListener("click", () => {
+  sidebarEl.style.display = "flex";
+  sidebarEl.style.boxShadow = "0 0 0 10000px rgba(0, 0, 0, 0.5)";
+});
 
 /********** For the slider **********/
 
 function showSlide(index) {
-  const slides = document.getElementById('slides'); //Gets the slider container
+  const slides = document.getElementById("slides"); //Gets the slider container
   const totalSlides = slides.children.length; // Gets the total number of slides
 
   // Wrap around if index goes out of bounds
@@ -49,31 +54,53 @@ function nextSlide() {
 // Initialize slider
 showSlide(currentIndex);
 
-
 /********** Plus and minus buttons **********/
 
-plusBtnEl.addEventListener('click',() => {
-    productQuantity += 1;
-    quantityEl.textContent = productQuantity;
+plusBtnEl.addEventListener("click", () => {
+  productQuantity += 1;
+  quantityEl.textContent = productQuantity;
+  minusBtnEl.disabled = false;
+});
+
+minusBtnEl.addEventListener("click", () => {
+  if (productQuantity === 0) {
+    minusBtnEl.disabled = true;
+  } else {
     minusBtnEl.disabled = false;
-})
+    productQuantity -= 1;
+    quantityEl.textContent = productQuantity;
+  }
+});
 
-minusBtnEl.addEventListener('click',() => {
-    if(productQuantity === 0) {
-        minusBtnEl.disabled = true;
-    }else{
-        minusBtnEl.disabled = false;
-        productQuantity -= 1;
-        quantityEl.textContent = productQuantity;
-    }
-    
-})
-
-/********** Cart Functionality **********/
+/********** Show Cart Functionality **********/
 cartBtnEl.addEventListener('click', () => {
-    if(cartContainerEl.style.display === 'none'){
-        cartContainerEl.style.display = 'block'
-    }else{
-        cartContainerEl.style.display = 'none'
-    }
+
+  emptyCartVisibility();
+
 })
+
+addToCartEl.addEventListener('click', () => {
+  
+  if(productQuantity > 0){
+    cartContainerEmptyEl.style.display = "none";
+  }
+
+})
+
+/********** Function for the cart functionality **********/
+function emptyCartVisibility(){
+  if(cartContainerEmptyEl.style.display === "none"){
+    cartContainerEmptyEl.style.display = "block"
+  }else{
+    cartContainerEmptyEl.style.display = "none";
+  }
+
+}
+
+function fullCartVisibility(){
+  if(cartContainerFullEl.style.display === "none"){
+    cartContainerFullEl.style.display = "block"
+  }else{
+    cartContainerFullEl.style.display = "none";
+  }
+}
